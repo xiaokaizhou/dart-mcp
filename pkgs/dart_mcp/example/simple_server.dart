f219861ec85c72c1be1f6518ae90d927cc50c607
Brett Morgan <brett.morgan@gmail.com>
2025-06-26 01:49:12 +1000

    Update `simple_server.dart` (#187)
    Make the example `simple_server.dart` work with Claude Desktop
    
    <img width="549" alt="Screenshot 2025-06-25 at 6 54 11 pm" src="https://github.com/user-attachments/assets/dc796e1e-d0a5-44cf-b7cb-8fdaca257bfe" />

--- pkgs/dart_mcp/example/simple_server.dart
+++ pkgs/dart_mcp/example/simple_server.dart
@@ -39,7 +39,7 @@ base class DartMCPServer extends MCPServer with ToolsSupport {
   @override
   FutureOr<InitializeResult> initialize(InitializeRequest request) {
     registerTool(
-      Tool(name: 'hello world', inputSchema: ObjectSchema()),
+      Tool(name: 'hello_world', inputSchema: ObjectSchema()),
       (_) => CallToolResult(content: [TextContent(text: 'hello world!')]),
     );
     return super.initialize(request);
